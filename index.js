f4c9c1c62ea05dac151e5b0b612dea997339abee
MashaMy <141316138+MashaMy@users.noreply.github.com>
2023-08-13 15:10:42 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+
+let countDownDate = new Date("June 15, 2023 00:00").getTime();
+
+// Update the count down every 1 second
+let x = setInterval(function() {
+
+    
+    let now = new Date().getTime();
+    
+    
+    let distance = countDownDate - now;
+    
+    let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+    document.getElementById("demo").innerHTML = days + " дней " + hours + " часов "
+    + minutes + " мин " + seconds + " секунд ";
+    
+    
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("demo").innerHTML = "Акция завершилась! To be continued!";
+    }
+}, 1000);
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
